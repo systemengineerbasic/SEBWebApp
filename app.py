3bcd4cb1bd0595a716603c57a195a2a490f35009
MasaoG <masao.goho@konicaminolta.com>
2019-04-18 13:10:47 +0900

    added template

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ from datetime import datetime
 @route("/")
 def hello_world():
     now = datetime.now()
-    return template('Hello シススク！{{now}}', now=now)
+    return template('html_template', now=now)
 
 run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
---
+++ views/html_template.tpl
@@ -0,0 +1,8 @@
+<html>
+    <body>
+    <ul>
+        <li> Hello シススク！</li>
+        <li> 現在の時刻は {{now}} です。</li>
+    </ul>
+    </body>
+</html>
