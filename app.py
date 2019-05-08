9bf9dfd21194ea3b73359b2666c8a4260c764492
MasaoG <masao.goho@konicaminolta.com>
2019-05-08 12:50:19 +0900

    Update static files

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
-import os
-from bottle import route, run, template, request
+import paho.mqtt.client as mqtt
+import os, urlparse
+from bottle import route, run, template, request, static_file
 from datetime import datetime
+
+#static file を読み込み、/static/ フォルダー内のファイル *.png 等を、./static/*.png と返す。
+@route('/static/<file_path:path>')
+def sever_static(file_path):
+    return static_file(file_path,root='./static/')
 
 @route("/")
 def hello_world():
--- views/html_template.tpl
+++ views/html_template.tpl
@@ -1,10 +1,16 @@
 <html>
     <body>
     <h2> シススク サンプル Web アプリケーション </h2>
+    <p>
     <ul>
         <li> 現在の信号機は、{{color}] 色です</li>
         <li> 現在の時刻は {{now}} 。</li>
     </ul>
+    </p>
+    <figure>
+    <legend> 信号機の状態 </legend>
+    <img src="./static/signal_full.png">
+    </figure>
     <form method="post" action="/">
         <input type="text" name="input_color">
         <input type="submit" value="MQTT_Publish">
