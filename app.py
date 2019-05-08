14ad638f2b2bdadc25581f8354450f8d26644cf1
MasaoG <masao.goho@konicaminolta.com>
2019-05-08 13:36:28 +0900

    fixed line 22

--- app.py
+++ app.py
@@ -18,12 +18,12 @@ def hello_world():
 def push_signal():
     now = datetime.now()
     input_color = request.forms.input_color
-    if input_color = red:
-        signal_color == '赤'
+    if input_color == red:
+        signal_color = '赤'
     elif input_color == yellow:
-        signal_color == '黄'
+        signal_color = '黄'
     else:
-        signal_color == '青'
+        signal_color = '青'
     return template('html_template', now=now, color='', scolor=signal_color)
 
 run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
